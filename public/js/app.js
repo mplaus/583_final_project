301875f88f6b592f06b263625caecd9f5b7cf2f6
plaus <plaus@live.unc.edu>
2016-04-23 14:15:17 -0400

    table load based on the onready value of menu selctor

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,64 @@ var app = angular.module('myApp', [ ]);
 
     app.controller('BaseController', ['$http', function ($http) {
         
-    this.salary_data_2015 = [ ];
-    var _this = this;
-    this.currentState = 0;
+        this.salary_data_2015 = [ ];
+        var _this = this;
     
-    $http.get('../data/2015_Salary_Data.json')
+    
+     
+     $( "#selectYear" ).ready(function() {
+      pickYear();
+    });
+     
+    
+    
+    //function allows user to pick year of data to load
+    function pickYear() {
+        
+        if (selectYear.options[selectYear.selectedIndex].value === "data-2015" ) {
+            
+        
+        
+        $http.get('../data/2015_Salary_Data.json')
         .success(function(data) {
             _this.salary_data_2015 = data;
             console.log(data);
+            return data
         })
         .error(function(msg) {
             console.log("Error");
         })
+        }
+    };
+    
+    //toggle function
+this.column = "Name";
+ 
+this.toggle = false;
 
+this.toggleIt = function(column) {
+   
+   
+   if (this.column == column) {
+     this.toggle = !this.toggle;
+   };
+   this.column = column;
+}
 
+this.column = "Salary";
+ 
+this.toggle = false;
+
+this.toggleIt = function(column) {
+   
+   
+   if (this.column == column) {
+     this.toggle = !this.toggle;
+   };
+   this.column = column;
+}
 
 
 }]);
+    
+    
