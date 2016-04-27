30d22d434f1e0f39b982f028f8015b94379a4af5
plaus <plaus@live.unc.edu>
2016-04-27 11:46:24 -0400

    Start the day

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,31 @@ var app = angular.module('myApp', [ ]);
     app.controller('BaseController', ['$http', function ($http) {
         
          var _this = this;
-         this.table_2015 = [ ];
-        
+         
+         function makeTable() {
+            this.data = [ ];
+         }
+         
+        function  changeTable() {
+            this.data = [];
+            
+        }
     
     
     
      //On document ready 2015 data is chosen from the default status of the selector then injected into table
         $( "#selectYear" ).ready(function() {
+         makeTable();
          pickYear();
        });
      
      //When user changes the year selector, deletes old table and loads new data set and injects data into new table
-        $( "#selectYear" ).change(function() {
-        $('.ng-binding').remove();
+      $( "#selectYear" ).change(function() {
+        //$('.ng-binding').remove();
+        changeTable();
         pickYear();
       });
-     
+
     
     
     //function allows user to pick year of data to load
@@ -30,9 +39,9 @@ var app = angular.module('myApp', [ ]);
         //loads 2015 data
         $http.get('../data/table_2015.json')
         .success(function(data) {
-            _this.table_2015 = data;
+            _this.data = data;
             console.log(data);
-            return data
+            //return data
         })
         .error(function(msg) {
             console.log("Error");
@@ -45,9 +54,9 @@ var app = angular.module('myApp', [ ]);
         //loads 2014 data
         $http.get('../data/table_2014.json')
         .success(function(data) {
-            _this.table_2014 = data;
+            _this.data = data;
             console.log(data);
-            return data
+            //return data
         })
         .error(function(msg) {
             console.log("Error");
@@ -69,20 +78,7 @@ this.toggleIt = function(column) {
    this.column = column;
 }
 
-/*this.column2 = "Salary";
- 
-this.toggle = false;
 
-this.toggleIt = function(column) {
-   
-   
-   if (this.column == column) {
-     this.toggle = !this.toggle;
-   };
-   this.column = column;
-}
-
-*/
 }]);
     
     
